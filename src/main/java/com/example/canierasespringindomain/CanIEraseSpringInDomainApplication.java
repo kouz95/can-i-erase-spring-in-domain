9d7b426510e960daa9d971307fc2d6dfb6b95946
kouz <kouz@kouzui-MacBookPro.local>
2021-06-26 16:27:41 +0900

    Initial commit

---
+++ src/main/java/com/example/canierasespringindomain/CanIEraseSpringInDomainApplication.java
@@ -0,0 +1,13 @@
+package com.example.canierasespringindomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanIEraseSpringInDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanIEraseSpringInDomainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/canierasespringindomain/CanIEraseSpringInDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.canierasespringindomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanIEraseSpringInDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
